a7d4059ca97da7fea9e4a9c9d891a6cc4eebab85
carson-loser <carson.loser@gmail.com>
2022-12-14 16:35:54 -0600

    added search bar and button

---
+++ assets/javascript/index.js
@@ -0,0 +1,7 @@
+var searchButton = document.querySelector('.searchButton');
+var inputValue = document.querySelector('.inputValue');
+var cityName = document.querySelector('.cityName');
+var cityDesc = document.querySelector('.cityDesc');
+var cityTemp = document.querySelector('.cityTemp');
+var cityHum = document.querySelector('.cityHum');
+var cityWind = document.querySelector('.windSpeed')
